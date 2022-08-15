bafb164884e806bd56904b0a74f6b07712c6d55f
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-15 15:23:19 +0000

    Mon, Aug 15, 2022, 3:23 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,9 +3,10 @@
 
 int main(void)
 {
-    int i = 
     do
     {
             int width = get_int("Enter width: ");
-    }while(i>0)
+            printf("#");
+            width--
+    }while(width>0);
 }
