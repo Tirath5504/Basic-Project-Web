df93fcaf8233a5debddc3acf7f3ea63a4bc437e9
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-16 16:01:02 +0000

    Tue, Aug 16, 2022, 4:01 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,8 +1,43 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Declaring Functions
+int input(void);
+void print_pattern(int n);
+
 // Main Function
 int main(void)
 {
-    
+    int width = input();
+    print_pattern(width);
+}
+
+// Taking input from user
+int input(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Enter Width: ");
+    }
+    while (n <= 0 || n > 8);
+    return n;
+}
+
+// Printing Pattern
+void print_pattern(int n)
+{
+    int c = (n / 2);
+    for(int i = 0; i < c; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            printf(" ");
+        }
+        for(int j = c; j >= (c - i); j--)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
