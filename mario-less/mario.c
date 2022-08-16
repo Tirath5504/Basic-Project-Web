476e345868bdd9a484bb791c3314c953d03979ae
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-16 14:59:43 +0000

    Tue, Aug 16, 2022, 2:59 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,16 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void input(void);
+
 int main(void)
 {
-    int width;
-    do
-    {
-         width = get_int("Width: ");
-    }while(width <= 0 );
-
     for(int i = width; i > 0; i--)
     {
+        input(void);
         for(int j = width; ; j++)
         {
                 if(j < )
