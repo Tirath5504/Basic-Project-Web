8199be367d2cdd92315dc76b137ba07a230fa287
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-16 07:02:34 +0000

    Tue, Aug 16, 2022, 7:02 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int get_number(void);
+
+int main(void)
 {
-    
+    int n = get_number(void);
+}
+
+int get_number(void)
+{
+    do
+    {
+        long n = get_long("Enter your credit card number");
+    }
+    while (n > 1000000000000);
 }
