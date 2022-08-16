b42d9a9cb80d609298a513a8a80e7ecfb5860de7
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-16 15:25:48 +0000

    Tue, Aug 16, 2022, 3:25 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -29,16 +29,10 @@ void print_pattern(int n)
 {
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n; j++)
+        for(int j = i; j < (n - 1); j++)
         {
-            if ()
-            {
-                 printf("#");
-            }
-            else
-            {
-                printf(" ");
-            }
+                 printf("*");
+
         }
         printf("\n");
     }
