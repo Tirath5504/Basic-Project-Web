adede37c81e559121434fac857714fc405fba917
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-25 07:46:31 +0000

    Thu, Aug 25, 2022, 7:46 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,7 +2,47 @@
 #include <stdio.h>
 #include <ctype.h>
 
+// Declaring Functions
+string input(void);
+string cipher(string s, int key);
+void output(string s);
+
 int main(int key)
 {
-    if(isdigit)
+    if(isdigit(key))
+    {
+        string  s = input();
+        string cipher_text = cipher(s, key);
+        void output(cipher_text);
+        return 0;
+    }
+    else
+    {
+        printf("Usage: ./caesar key");
+        return 1;
+    }
+}
+
+// Taking Plain Text
+string input(void)
+{
+    string s = get_string("Plain Text:  ");
+    return s;
+}
+
+// Converting to Cipher Text
+string cipher(string s, int key)
+{
+    int n = strlen(s);
+    for(int i = 0; i < n; i++)
+    {
+        s[i] = s[i] + key;
+    }
+    return s;
+}
+
+// Printing Output
+void output(string s)
+{
+    printf(")
 }
