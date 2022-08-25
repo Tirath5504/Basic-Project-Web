f31b5671b4879f8399382f0c8ee3eeb726cf1ace
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-25 05:37:48 +0000

    Thu, Aug 25, 2022, 5:37 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,19 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Declaring Functions
 string input(void);
-int letters(string s);
+int length(string s);
+int letters(string s, int n);
 int words(string s);
 int sentences(string s);
+void output(int n1, int n2, int n3);
 
 // Main Function
 int main(void)
 {
     string s = input();
-    int n_l = letters(s);
-    int n_w = words(s);
-    int n_s = sentences(s);
+    int n = length(s);
+    int n_l = letters(s,n);
+    int n_w = words(s,n);
+    int n_s = sentences(s,n);
     output(n_l, n_w, n_s);
 }
 
@@ -23,9 +27,28 @@ string input(void)
     return get_string("Enter your text: \n");
 }
 
+// Computing Length of text
+int length(string s);
+{
+    return strlen(s);
+}
+
 // Counting letters
-int letters(string s);
+int letters(string s,int n)
 {
     int n1 = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(isalphabet(s[i]))
+        {
+            n1++;
+        }
+    }
+    return n1;
+}
 
+// Counting words
+int words(string s, int n)
+{
+    int n1 = 
 }
