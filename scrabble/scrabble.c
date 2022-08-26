d72c1325bf30fbf80622ed1b5439f632a594a156
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-26 07:48:28 +0000

    Fri, Aug 26, 2022, 7:48 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -22,11 +22,11 @@ int main(void)
     // TODO: Print the winner
     if (score1 > score2)
     {
-        printf("Players 1 wins!\n");
+        printf("Player 1 wins!\n");
     }
     else if (score1 < score2)
     {
-        printf("Players 2 wins!\n");
+        printf("Player 2 wins!\n");
     }
     else
     {
