6dac0169713d0dc72767024b56fbba9952388d21
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-28 06:06:38 +0000

    Sun, Aug 28, 2022, 6:06 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,7 +127,13 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+    for(int i = 0; i < candidate_count; i++)
+    {
+        if(strcmp(name,candidates[i].name) == 0)
+        {
+            return true;
+        }
+    }
     return false;
 }
 
