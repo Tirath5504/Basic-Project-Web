b05ecc91e0f41c04b8cb98ec3868c9d81376ff45
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-28 07:39:21 +0000

    Sun, Aug 28, 2022, 7:39 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -178,7 +178,7 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    int c = MAX_VOTERS;
+    int c = 100;
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].votes < c)
