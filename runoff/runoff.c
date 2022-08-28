bd1fb7815f08203ff1ea217121cd2fda64b6ba68
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-28 06:30:59 +0000

    Sun, Aug 28, 2022, 6:30 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -151,6 +151,7 @@ void tabulate(void)
                  if (strcmp(candidate[j].name, preference[i][j] == 0))
                  {
                     candidate[j].votes++;
+                    break;
                  }
             }
         }
@@ -161,7 +162,15 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    int maj = voter_count / 2;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > maj)
+        {
+            printf("%s", candidates[i].name);
+            return true;
+        }
+    }
     return false;
 }
 
