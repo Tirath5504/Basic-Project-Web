ccfd1e6781a55dc6698b648ff417018f625e49c4
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-28 06:35:50 +0000

    Sun, Aug 28, 2022, 6:35 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -177,14 +177,21 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+    int c = MAX_VOTERS;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if(candidates[i].votes < c)
+        {
+            c = candidates[i].votes;
+        }
+    }
+    return c;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
+
     return false;
 }
 
