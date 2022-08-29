f85c1126d5a8abc2dc5ca01c3d2735468e1f7a9d
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-29 07:01:14 +0000

    Mon, Aug 29, 2022, 7:01 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -123,7 +123,7 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    for(int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
         for (int j = i; j < candidate_count; j++)
         {
