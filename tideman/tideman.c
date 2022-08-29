dddfbc05195a012804692fb4fa3add2c48402664
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-29 14:46:54 +0000

    Mon, Aug 29, 2022, 2:46 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -132,7 +132,7 @@ void add_pairs(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (preferences[i][j] > 0)
+            if (preferences[i][j] > preferences[j][i])
             {
                 pairs[j].winner = i;
                 pairs[i].loser = j;
