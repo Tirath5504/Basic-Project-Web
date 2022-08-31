bebc23c28edef9c0af4124e9b32638ef5a51e708
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-08-31 07:14:36 +0000

    Wed, Aug 31, 2022, 7:14 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -36,6 +36,7 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
 
     uint8_t header[HEADER_SIZE];
+    int16_t buffer;
     
 
     // TODO: Read samples from input file and write updated data to output file
