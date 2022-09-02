52b9b0962743b201114e7bcba52520b1c0b4add7
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-02 05:23:27 +0000

    Fri, Sep 2, 2022, 5:23 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -8,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             int avg = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0;
-            
+            image[i][j] = max(0, min(255, avg));
         }
     }
     return;
