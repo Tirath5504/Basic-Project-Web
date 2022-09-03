6e7273d252dee06bb2d0c188653b0a8f9844cd11
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-03 06:23:03 +0000

    Sat, Sep 3, 2022, 6:23 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main(int argc, char *argv[])
 {
+    // Checking if user input is valid
+
     if (argc != 2)
     {
         printf("Usage: ./recover IMAGE\n");
         return 1;
     }
-    
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)\
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    // C
     return 0;
 }
