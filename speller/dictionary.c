e6dab5eb01d34fd592c1897cfcd4906ee93faed7
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-09 14:32:01 +0000

    Fri, Sep 9, 2022, 2:32 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -32,7 +32,7 @@ bool load(const char *dictionary)
     {
         count[j] = 0;
     }
-    FILE *file = fopen("/workspaces/111230032/speller/dictionaries/large", "r");
+    FILE *file = fopen(dictionary, "r");
     if (file == NULL)
     {
         return false;
