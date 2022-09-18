77948fac208b4c65576f84234909d4a10165ff76
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 17:54:41 +0000

    Sun, Sep 18, 2022, 5:54 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,31 +1,31 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Checking the crime scene report for the given crime
-SELECT *
+/*SELECT *
     FROM crime_scene_reports
     WHERE month = 7
     AND day = 28
-    AND street = "Humphrey Street";
+    AND street = "Humphrey Street";*/
 -- Mention of three witnesses and bakery
 
 -- Checking interviews on 28 July for first witness
-SELECT *
+/*SELECT *
     FROM interviews
     WHERE month = 7
-    AND day = 28;
+    AND day = 28;*/
 -- Thief exited bakery within 10 mins of 10:15 AM
 
 -- Checking bakery security logs for given day and time
-SELECT *
+/*SELECT *
     FROM bakery_security_logs
     WHERE day = 28
     AND hour = 10
     AND minute > 14
-    AND minute < 26;
+    AND minute < 26;*/
 -- Many people exited during the given time
 
 -- Reducing possible thief prospects (with help of license plate) (1)
-SELECT *
+/*SELECT *
     FROM people
     WHERE license_plate IN
     (SELECT license_plate
@@ -33,27 +33,27 @@ SELECT *
     WHERE day = 28
     AND hour = 10
     AND minute > 14
-    AND minute < 26);
+    AND minute < 26);*/
 -- Reduced number of prospects
 
 -- Checking interviews on 28 July again for second witness
-SELECT *
+/*SELECT *
     FROM interviews
     WHERE month = 7
-    AND day = 28;
+    AND day = 28;*/
 -- Thief went to atm on Leggett Street
 
 -- Checking atm transactions on 28 july before 10 15 am
-SELECT *
+/*SELECT *
     FROM atm_transactions
     WHERE day = 28
     AND month = 7
     AND atm_location = "Leggett Street"
-    AND transaction_type = "withdraw";
+    AND transaction_type = "withdraw";*/
 -- Several prospects found
 
 -- Reducing possible thief prospects (with help of account number) (2)
-SELECT *
+/*SELECT *
     FROM people
     WHERE id IN
     (SELECT person_id
@@ -74,22 +74,22 @@ SELECT *
         WHERE day = 28
         AND hour = 10
         AND minute > 14
-        AND minute < 26);
+        AND minute < 26);*/
 -- Reduced number of prospects
 
 -- Checking interviews on 28 July again for third witness
-SELECT *
+/*SELECT *
     FROM interviews
     WHERE month = 7
-    AND day = 28;
+    AND day = 28;*/
 -- Thief called during exit for a duration of less than a minute
 
 -- Checing phone calls on 28 july 2021 for duration of less than a minute
-SELECT *
+/*SELECT *
     FROM phone_calls
     WHERE month = 7
     AND day = 28
-    AND duration < 60;
+    AND duration < 60;*/
 -- Got some caller information
 
 -- Reducing possible thief prospects (with help of phone number) (3)
@@ -127,7 +127,7 @@ SELECT *
 -- Reduced number of prospects
 
 -- Checking number of people whose flight departed from Fiftyville after 10 am on 28 july
-SELECT *
+/*SELECT *
     FROM people
     WHERE passport_number IN
         (SELECT passport_number
@@ -140,11 +140,11 @@ SELECT *
                         WHERE city = "Fiftyville")
                     AND day = 29
                     AND month = 7
-                    AND hour > 10));
+                    AND hour > 10));*/
 -- Got some passenger details
 
 -- Reducing possible thief prospects (with help of passport number) (4)
-SELECT *
+/*SELECT *
     FROM people
     WHERE id IN
     (SELECT person_id
@@ -189,11 +189,11 @@ SELECT *
                         WHERE city = "Fiftyville")
                     AND day = 29
                     AND month = 7
-                    AND hour > 10));
+                    AND hour > 10));*/
 -- Got the criminal!
 
 -- Finding which city the thief escaped to
-SELECT city
+/*SELECT city
     FROM airports
     WHERE id =
         (SELECT destination_airport_id
@@ -201,11 +201,11 @@ SELECT city
             WHERE id =
                 (SELECT flight_id
                     FROM passengers
-                    WHERE passport_number = 3592750733));
+                    WHERE passport_number = 3592750733));*/
 -- Found the city the thief escaped to!
 
 -- Finding accomplise through phone call
-SELECT *
+/*SELECT *
     FROM people
     WHERE phone_number =
         (SELECT receiver
@@ -213,5 +213,5 @@ SELECT *
             WHERE month = 7
             AND day = 28
             AND duration < 60
-            AND caller = "(770) 555-1861");
+            AND caller = "(770) 555-1861");*/
 -- Got the accomplice!
