46e3d9c0c53c1f31832e195315e2322c47bac4ea
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 14:12:21 +0000

    Sun, Sep 18, 2022, 2:12 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,6 +1,6 @@
 SELECT title FROM movies
 WHERE id IN
-    (SELECT movie_id FROM stars
+    (SELECT DISTINCT movie_id FROM stars
     WHERE person_id =
         (SELECT id FROM people
         WHERE name = "Johnny Depp")
