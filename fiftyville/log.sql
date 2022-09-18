ac1dd3111a24e5f3f276b8d14495a93e60d087ef
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 15:31:04 +0000

    Sun, Sep 18, 2022, 3:31 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,28 +6,40 @@
     WHERE month = 7
     AND day = 28
     AND street = "Humphrey Street";*/
+-- Mention of three witnesses and bakery
 
 -- Checking interviews on 28 July for first witness
 /*SELECT *
     FROM interviews
     WHERE month = 7
     AND day = 28;*/
+-- Thief exited bakery within 10 mins of 10:15 AM
 
 -- Checking bakery security logs for given day and time
 /*SELECT *
     FROM bakery_security_logs
     WHERE day = 28
     AND hour = 10;*/
+-- Many people exited during the given time
 
 -- Checking interviews on 28 July again for second witness
 /*SELECT *
     FROM interviews
     WHERE month = 7
     AND day = 28;*/
+-- Thief went to atm on Leggett Street
 
 -- Checking atm transactions on 28 july before 10 15 am
-SELECT *
+/*SELECT *
     FROM atm_transactions
     WHERE day = 28
     AND month = 7
-    AND atm_location = "Leggett Street";
+    AND atm_location = "Leggett Street";*/
+-- No paricular information obtained
+
+-- Checking interviews on 28 July again for third witness
+SELECT *
+    FROM interviews
+    WHERE month = 7
+    AND day = 28;
+-- Thief went to atm on Leggett Street
