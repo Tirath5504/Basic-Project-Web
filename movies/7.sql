a24027418c1aea39eb4ae733f56b09b9764c2481
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 04:30:12 +0000

    Sun, Sep 18, 2022, 4:30 AM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,10 +1,10 @@
-SELECT movies.title, ratings.rating, COUNT(DISTINCT title)
+SELECT movies.title, ratings.rating, COUNT(DISTINCT title) AS a
 FROM movies
 JOIN ratings
 ON movies.id = ratings.movie_id
 WHERE year = 2010
 GROUP BY title
 (CASE
-    HAVING COUNT(rating) = 1 THEN ORDER BY rating
+    WHEN COUNT(a) = 1 THEN ORDER BY rating
     ELSE ORDER BY title
 END);
