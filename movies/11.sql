59b5f5771fdb8e4b2b2541c412509afb852b99b1
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 05:06:25 +0000

    Sun, Sep 18, 2022, 5:06 AM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,4 +1,4 @@
-SELECT title FROM movies JOIN ratings ON ratings.rating = movies.year
+SELECT title FROM movies JOIN ratings ON ratings.rating = movies.rating
 WHERE id IN
 (SELECT movie_id FROM stars
 WHERE person_id IN
