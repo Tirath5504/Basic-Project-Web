a8e4e36391ae28d90b243271caa16e6476829829
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 15:18:15 +0000

    Sun, Sep 18, 2022, 3:18 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,8 +7,14 @@
     AND day = 28
     AND street = "Humphrey Street";*/
 
--- Checking bakery security logs for given day and time
+-- Checking interviews on 28 July
 SELECT *
+    FROM interviews
+    WHERE month = 7
+    AND day = 28;
+
+-- Checking bakery security logs for given day and time
+/*SELECT *
     FROM bakery_security_logs
     WHERE day = 28
-    AND hour = 10;
+    AND hour = 10;*/
