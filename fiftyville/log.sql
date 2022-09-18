16bfa2d76a4f575e5ee9d24e83ca93a1ca35a313
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 15:12:23 +0000

    Sun, Sep 18, 2022, 3:12 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,14 +1,14 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Checking the crime scene report for the given crime
-SELECT *
+/*SELECT *
     FROM crime_scene_reports
     WHERE month = 7
     AND day = 28
-    AND street = "Humphrey Street";
+    AND street = "Humphrey Street";*/
 
 -- Checking bakery security logs for given day and time
-/*SELECT *
+SELECT *
     FROM bakery_security_logs
     WHERE day = 28
-    AND hour = 10;*/
+    AND hour = 10;
