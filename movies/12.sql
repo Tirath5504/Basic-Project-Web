2288f246fb559d1b80058fd62df2ab25e6ead862
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 14:35:54 +0000

    Sun, Sep 18, 2022, 2:35 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -11,3 +11,21 @@ WHERE id IN
     WHERE person_id =
         (SELECT id FROM people
         WHERE name = "Helena Bohman Carter"))
+
+
+SELECT movies.title
+FROM movies
+JOIN people
+  ON people.id = stars.person_id
+JOIN stars
+  ON stars.movie_id = movies.id
+WHERE people.name = "Johnny Depp"
+INTERSECT
+SELECT movies.title
+FROM movies
+JOIN people
+ON people.id = stars.person_id
+JOIN stars
+ON stars.movie_id = movies.id
+WHERE people.name = "Helena Bonham Carter"
+ORDER BY movies.title
