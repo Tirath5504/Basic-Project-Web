ce461563cf37d79bcc47c0dad6a4fa481369ac85
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 05:42:42 +0000

    Sun, Sep 18, 2022, 5:42 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -4,6 +4,6 @@ WHERE id IN
     WHERE person_id IN
         (SELECT id FROM people
         WHERE name = "Johnny Depp")
-        AND person_id IN
-            (SELECT id FROM people
-            WHERE name = "Helena Bonham Carter"));
+    AND person_id IN
+        (SELECT id FROM people
+        WHERE name = "Helena Bonham Carter"));
