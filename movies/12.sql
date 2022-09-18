c198647b166d6beecc9311a58cc0bf6fc42920ba
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 14:32:49 +0000

    Sun, Sep 18, 2022, 2:32 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -3,6 +3,11 @@ WHERE id IN
     (SELECT movie_id FROM stars
     WHERE person_id =
         (SELECT id FROM people
-        WHERE name IN ("Johnny Depp", "Helena Bohman Carter"))
-    GROUP BY person_id
-    HAVING COUNT(person_id) = 2);
+        WHERE name = "Johnny Depp"))
+INTERSECT
+SELECT title FROM movies
+WHERE id IN
+    (SELECT movie_id FROM stars
+    WHERE person_id =
+        (SELECT id FROM people
+        WHERE name = "Helena Bohman Carter"))
