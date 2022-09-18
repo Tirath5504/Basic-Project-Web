e8c9f11f7423a2d3e569978115de80036cdcec57
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 15:00:01 +0000

    Sun, Sep 18, 2022, 3:00 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+SELECT *
+    FROM crime_scene_reports
+    WHERE month = July
+    AND year = 2021
+    AND street = "Humphrey Street";
