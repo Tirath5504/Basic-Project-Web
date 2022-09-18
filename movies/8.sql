2f872fcf34bc6a60f064ef9040a953c8efcee556
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 04:34:50 +0000

    Sun, Sep 18, 2022, 4:34 AM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -2,4 +2,5 @@ SELECT name FROM people
 WHERE id IN
 (SELECT person_id FROM stars
 WHERE movie_id =
-(SELECT id FROM movies WHERE title = "Toy Story"));
+(SELECT id FROM movies
+WHERE title = "Toy Story"));
