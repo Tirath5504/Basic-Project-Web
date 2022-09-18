42f32dfda3c3dea4c78c8409438d82c8120ed106
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 13:08:51 +0000

    Sun, Sep 18, 2022, 1:08 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,8 +1,8 @@
-SELECT title FROM movies JOIN ratings ON ratings.rating = movies.rating
+SELECT title FROM movies
+JOIN ratings
+ON ratings.rating = movies.rating
 WHERE id IN
-(SELECT movie_id FROM stars
-WHERE person_id IN
-(SELECT id FROM people
-WHERE name = "Chadwick Boseman"))
-ORDER BY ratings.rating DESC
-LIMIT 5;
+    (SELECT movie_id FROM stars
+    WHERE person_id IN
+        (SELECT id FROM people
+        WHERE name = "Chadwick Boseman")
