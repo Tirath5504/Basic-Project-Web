4b79c5fbf9786d76d0b2931e0d5978478ed0e20f
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-09-18 14:03:20 +0000

    Sun, Sep 18, 2022, 2:03 PM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,10 +1,6 @@
-SELECT movies.title, ratings.rating
+SELECT movies.title, DISTINCT (ratings.rating)
 FROM movies
 INNER JOIN ratings
 ON ratings.movie_id = movies.id
 WHERE year = 2010
-ORDER BY
-(CASE
-    WHEN COUNT(rating) = 1 THEN rating DESC
-    ELSE title
-END);
+ORDER BY rating DESC;
