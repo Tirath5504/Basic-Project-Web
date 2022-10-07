973d9b6b44a60dd296c842ac1a8fd9c8360c07e5
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-10-07 06:25:25 +0000

    Fri, Oct 7, 2022, 6:25 AM +00:00

---
+++ codeforces/pset2-waytoolongwords.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    string str;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++)
+    {
+        scanf("%s", &str);
+        
+    }
+}
