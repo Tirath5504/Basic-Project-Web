a45d84f3dc3a7b93d39ff9f88bfe44a53125f899
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-10-07 06:30:45 +0000

    Fri, Oct 7, 2022, 6:30 AM +00:00

--- codeforces/pset2-waytoolongwords.c
+++ codeforces/pset2-waytoolongwords.c
@@ -1,15 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     int n;
-    string str;
     printf("Enter a number: ");
     scanf("%d", &n);
-    for(int i = 1; i <= n; i++)
+    string str[n];
+    for(int i = 0; i < n; i++)
     {
-        scanf("%s", &str);
-        
+        scanf("%s", str[i]);
+    }
+    for(int i = 0; i < n; i++)
+    {
+        int len = str[i].length();
+        if (len > 10)
+        {
+            string a = str[0] + len + str[len];
+        }
+        else
+            printf(str[i]);
     }
 }
