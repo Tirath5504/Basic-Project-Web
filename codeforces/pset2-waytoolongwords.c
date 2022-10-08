ce326fda632f166cea1b0a80ab760d832a62c67f
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-10-08 12:48:43 +0000

    Sat, Oct 8, 2022, 12:48 PM +00:00

--- codeforces/pset2-waytoolongwords.c
+++ codeforces/pset2-waytoolongwords.c
@@ -18,9 +18,12 @@ int main(void)
         if (len > 10)
         {
             char *ans = malloc(50 * sizeof(char));
-            strcat(ans, &str[0]);
+            char *first = &str[0];
+            strcat(ans, first);
             strcat(ans, length);
             strcat(ans, &str[len - 1]);
+            printf("%s", ans);
+            free(ans);
         }
         else
             printf("%s\n", str);
