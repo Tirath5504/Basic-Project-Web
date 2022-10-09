3cde2f22a7ab018d97053f19feee4d7f9724adf6
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-10-09 12:46:41 +0000

    Sun, Oct 9, 2022, 12:46 PM +00:00

--- codeforces/pset3-team.c
+++ codeforces/pset3-team.c
@@ -1,12 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
 int main(void)
 {
     int n;
-    char *str = malloc(10*sizeof(char));
     do
     {
         printf("Enter number of problems (between 1 and 1000): ");
@@ -14,24 +11,18 @@ int main(void)
     }
     while (n > 1000 || n < 0);
     int c2 = 0;
-    char ch;
+    int num[3];
     for (int i = 0; i < n; i++)
     {
+        scanf("%d%d%d", &num[0], &num[1], &num[2]);
+        int c1 = 0;
         for (int j = 0; j < 3; j++)
         {
-            scanf("%s", &ch);
-            strncat(str, &ch, 1);
-        }
-        int c1 = 0;
-        for (int j = 0; j < strlen(str); j++)
-        {
-            if (str[j] == 1)
+            if (num[j] == 1)
                 c1++;
         }
-        if (c1 > 2)
+        if (c1 >= 2)
             c2++;
-        str = "";
     }
     printf("%d\n", c2);
-    free(str);
 }
