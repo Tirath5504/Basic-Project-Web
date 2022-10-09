897920e791a0d957925b4db507898e33d73675cc
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-10-09 08:11:05 +0000

    Sun, Oct 9, 2022, 8:11 AM +00:00

--- codeforces/pset3-team.c
+++ codeforces/pset3-team.c
@@ -1,1 +1,17 @@
 #include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        printf("Enter number of problems (between 1 and 1000): ");
+        scanf("%d", &n);
+    }
+    while (n > 1000 || n < 0);
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
+}
