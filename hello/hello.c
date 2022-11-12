55572532151efc9bb438cf3b817a1256a5070037
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-11-12 05:28:06 +0000

    Sat, Nov 12, 2022, 5:28 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -4,6 +4,6 @@
 int main(void)
 {
     string name = get_string("What's your name? ");  // To get name from user
-    printf("Hello, World\n");  //Printing Hello World
-    printf("Hello, %s \n", name);  //Printing Hello + username
+    printf("Hello, World\n");  // Printing Hello World
+    printf("Hello, %s \n", name);  // Printing Hello + username
 }
