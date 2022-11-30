6b9f71b2ce0f478eea43c6db78c668fa4d756acb
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-11-30 15:44:27 +0000

    Wed, Nov 30, 2022, 3:44 PM +00:00

--- javatpoint/practise_for_hackerrank.c
+++ javatpoint/practise_for_hackerrank.c
@@ -10,8 +10,8 @@ int main()
     char ch;
     scanf("%c", &ch);
     printf("%c", ch);
-    char s;
-    scanf("%[^\n]%s", &s);
+    char s[MAX_LEN];
+    scanf("%s", &s);
     printf("%c", s);
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     return 0;
