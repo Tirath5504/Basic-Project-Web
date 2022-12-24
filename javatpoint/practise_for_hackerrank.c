f6b7a213e4c8a4d3f599506abc2dab7ded9f5f08
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2022-12-24 17:28:09 +0000

    Sat, Dec 24, 2022, 5:28 PM +00:00

--- javatpoint/practise_for_hackerrank.c
+++ javatpoint/practise_for_hackerrank.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-    int n, *arr, i;
-    scanf("%d", &n);
-    arr = (int*) malloc(n * sizeof(int));
-    for(i = 0; i < n; i++) {
-        scanf("%d", arr + i);
-    }
-    int *arr2 = NULL;;
-    for (i = 0; i < n; i++)
-    {
-        arr2[i] = *arr[n - i - 1];
-    }
-    for (i = 0; i < n; i++)
-    {
-        arr[i] = *arr2[i];
-    }
-    /* Write the logic to reverse the array. */
-
-    for(i = 0; i < n; i++)
-        printf("%d ", *(arr + i));
-    return 0;
+	int tc;
+	scanf("%d", &tc);
+	int num[tc];
+	for (int i = 0; i < tc; i++)
+	{
+		scanf("%d", &num[i]);
+	}
+	for (int i = 0; i < tc; i++)
+	{
+		int count = 0;
+		for (int j = 1; j <= tc; j++)
+		{
+			int div = num[i] / j;
+			if (sizeof(div) < sizeof(num[i]))
+			{
+				count++;
+			}
+		}
+		printf("%d\n", count);
+	}
 }
