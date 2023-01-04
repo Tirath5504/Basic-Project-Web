45a47006f661d66c0c94f4a28619525e429d8664
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-01-04 07:55:31 +0000

    Wed, Jan 4, 2023, 7:55 AM +00:00

--- javatpoint/practise_for_hackerrank.c
+++ javatpoint/practise_for_hackerrank.c
@@ -5,14 +5,11 @@
 int main(void)
 {
 	int n1;
-	clrscr();
 	printf("Enter a number : ");
 	scanf("%d", &n1);
 	if (n1 % 2 == 0)
 		printf("Even number\n");
 	else
 		printf("Odd number\n");
-	printf("Numbers after swap : %d, %d", &n1, &n2);
-	getch();
 	return 0;
 }
