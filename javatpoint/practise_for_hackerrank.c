ca0f8be87edac5dadd0274e497ad925bc8aab079
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-01-04 07:54:04 +0000

    Wed, Jan 4, 2023, 7:54 AM +00:00

--- javatpoint/practise_for_hackerrank.c
+++ javatpoint/practise_for_hackerrank.c
@@ -1,26 +1,18 @@
-#include <stdio.h>
+// WAP to swap variables with help of three variables
 
-int main()
+#include<stdio.h>
+
+int main(void)
 {
-	int tc;
-	scanf("%d", &tc);
-	int num[tc];
-	for (int i = 0; i < tc; i++)
-	{
-		scanf("%d", &num[i]);
-	}
-	for (int i = 0; i < tc; i++)
-	{
-		int count = 0;
-		for (int j = 1; j <= num[i]; j++)
-		{
-			int div = num[i] / j;
-            int a = sizeof(div), b = sizeof(j);
-			if (a < b)
-			{
-				count++;
-			}
-		}
-		printf("%d\n", count);
-	}
+	int n1;
+	clrscr();
+	printf("Enter a number : ");
+	scanf("%d", &n1);
+	if (n1 % 2 == 0)
+		printf("Even number\n");
+	else
+		printf("Odd number\n");
+	printf("Numbers after swap : %d, %d", &n1, &n2);
+	getch();
+	return 0;
 }
