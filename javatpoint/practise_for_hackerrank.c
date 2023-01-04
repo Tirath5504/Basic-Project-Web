a414979d60210032ef64a86e3b1856d8006707b8
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-01-04 08:11:45 +0000

    Wed, Jan 4, 2023, 8:11 AM +00:00

--- javatpoint/practise_for_hackerrank.c
+++ javatpoint/practise_for_hackerrank.c
@@ -7,10 +7,11 @@ int main(void)
 	int n1;
 	printf("Enter a number : ");
 	scanf("%d", &n1);
-	if (n1 % 100 == 0)
-	{
-		if (n1 % 400 == 0)
-			printf(")
-	}
+	if (n1 % 100 == 0 && n1 % 400 == 0)
+		printf("Leap Year\n");
+	else if (n1 % 4 == 0 && n1 % 100 != 0)
+		printf("Leap Year\n");
+	else
+		printf("Not a leap Year\n");
 	return 0;
 }
