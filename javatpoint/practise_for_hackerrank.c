5c94d8179fb907140058a137e5b31b24f1c1d909
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-01-04 08:05:26 +0000

    Wed, Jan 4, 2023, 8:05 AM +00:00

--- javatpoint/practise_for_hackerrank.c
+++ javatpoint/practise_for_hackerrank.c
@@ -7,6 +7,10 @@ int main(void)
 	int n1;
 	printf("Enter a number : ");
 	scanf("%d", &n1);
-	n1 % 2 ? printf("Odd number\n"): printf("Even Number\n");
+	if (n1 % 100 == 0)
+	{
+		if (n1 % 400 == 0)
+			printf(")
+	}
 	return 0;
 }
