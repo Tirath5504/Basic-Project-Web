558edd36fa21b18165f3163fa0d6239b62010538
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-01-11 17:37:09 +0000

    Wed, Jan 11, 2023, 5:37 PM +00:00

--- javatpoint/practise_for_hackerrank.c
+++ javatpoint/practise_for_hackerrank.c
@@ -11,6 +11,18 @@ int main(void)
 		{
 			printf("*");
 		}
+		for (int k = 1; k <= i; k++)
+		{
+			printf(" ");
+		}
+		for (int l = 1; l <= i; l++)
+		{
+			printf("*");
+		}
+		for (int m = 1; m <= i; m++)
+		{
+			printf("*");
+		}
 		printf("\n");
 	}
 	return 0;
