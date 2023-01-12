178078045a99e699fcee517903a1dceb9b0221e1
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-01-12 10:14:53 +0000

    Thu, Jan 12, 2023, 10:14 AM +00:00

--- javatpoint/practise_for_hackerrank.c
+++ javatpoint/practise_for_hackerrank.c
@@ -7,14 +7,6 @@ int main(void)
 	scanf("%d", &n);
 	for (int i = 1; i <= n; i++)
 	{
-		for (int j = 1; j <= i; j++)
-		{
-			printf("*");
-		}
-		for (int k = n; k >= i; k--)
-		{
-			printf(" ");
-		}
 		for (int l = n; l > i; l--)
 		{
 			printf(" ");
