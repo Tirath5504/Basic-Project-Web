c067d67ff8e66ac88e11ee01c91288aaa69234e9
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-01-21 06:22:19 +0000

    Sat, Jan 21, 2023, 6:22 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,17 +6,20 @@ from pyfiglet import Figlet
 def main():
 
     # Checking input from command-line
-    if not len(sys.argv) == 1 or not len(sys.argv) == 3:
+    if len(sys.argv) == 1 or len(sys.argv) == 3:
+
+        # Taking input text
+        text = input("Input: ")
+        figlet = Figlet()
+        print(figlet.getFonts())
+
+        # If no command-line argument then print text in random font
+        if len(sys.argv) == 1:
+            print("Output: ")
+
+    else:
+
         print("Invalid usage")
         sys.exit()
-
-    # Taking input text
-    text = input("Input: ")
-    figlet = Figlet()
-    print(figlet.getFonts())
-
-    # If no command-line argument then print text in random font
-    if len(sys.argv) == 1:
-        print("Output: ")
 
 main()
