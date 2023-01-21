4a513db60f19fc605c02d5ce5d295ddb89e2af70
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-01-21 05:36:44 +0000

    Sat, Jan 21, 2023, 5:36 AM +00:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,4 +1,10 @@
 # Week 6 - Python - Practise Problems
 
+# Importing get_string from cs50 to get input
+from cs50 import get_string
+
 # Getting Input
-while
+while True:
+    input = get_string("Input: ")
+
+if 
