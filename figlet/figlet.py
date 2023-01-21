25a5714c24ea077073c9c9048460e335804e3f8b
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-01-21 06:08:29 +0000

    Sat, Jan 21, 2023, 6:08 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,7 +1,12 @@
 # cs50 - Week 6 - Python - Figlet
 
-
+import sys
 
 def main():
 
-    # Getting
+    # Checking input from command-line
+    if not len(sys.argv) == 1 or not len(sys.argv) == 3:
+        print("Invalid usage")
+        sys.exit
+
+    
