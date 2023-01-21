2ff07e783aaed9afbd84f5848054a75537fcaf0a
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-01-21 06:31:59 +0000

    Sat, Jan 21, 2023, 6:31 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -11,12 +11,29 @@ def main():
         # Taking input text
         text = input("Input: ")
         figlet = Figlet()
-        for i in figlet.getFonts():
-            print(i)
+        print("Output: ")
 
         # If no command-line argument then print text in random font
         if len(sys.argv) == 1:
-            print("Output: ")
+            figlet.setFont(font = rand)
+
+        # If two command-line arguments are inputted
+        elif len(sys.argv) == 3:
+
+            # Checking if second argument is made to change font
+            if sys.argv[1] == "-f" or sys.argv[1] == "--font":
+
+                # Checking if input font is in figlet font list
+                if sys.argv[2] in figlet.getFonts():
+                    figlet.setFont(font = sys.argv[2])
+
+                else:
+                    print("Invalid input")
+
+            else:
+                print("Invalid input")
+
+        print(figlet.renderText(text))
 
     else:
 
