a3313287b9b8b67bd75f4c8716dfc753e8a4a25a
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-01-30 17:16:28 +0000

    Mon, Jan 30, 2023, 10:46 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 
     if (argc != 2)
     {
-        printf("Usage: ./recover IMAGE\n");
+        printf("Usage: ./recover card.raw\n");
         return 1;
     }
     FILE *file = fopen(argv[1], "r");
@@ -22,36 +22,38 @@ int main(int argc, char *argv[])
 
     // Checking if input is JPEG file
 
-    int16_t buffer[512];
+    uint8_t buffer[BLOCK_SIZE];
     int i = 0;
-    char *filename = malloc(10000);
-    while (fread(buffer, 1, BLOCK_SIZE, file) != EOF)
+    char filename[8];
+    FILE *img;
+    while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             if (i == 0)
             {
                 sprintf(filename, "%03i.jpg", i);
-                FILE *img = fopen(filename, "w");
-                fwrite(file, BLOCK_SIZE, 1, img);
+                img = fopen(filename, "w");
+                fwrite(buffer, BLOCK_SIZE, 1, img);
                 fclose(img);
                 i++;
             }
             else
             {
-                FILE *img = fopen(filename, "w");
+                fclose(img);
+                sprintf(filename, "03i.jpg", (n - 1));
+                img = fopen(filename, "w");
                 fwrite(file, BLOCK_SIZE, 1, img);
                 fclose(img);
-                i++;
             }
         }
         else
         {
-            FILE *img = fopen(filename, "w");
-            fwrite(file, BLOCK_SIZE, 1, img);
+            fwrite(img, BLOCK_SIZE, 1, file);
             fclose(img);
         }
     }
+    fclose(file);
     free(filename);
     return 0;
 }
