934e077190ae0e457e044c51b7da9467832a66d4
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-01-30 17:53:31 +0000

    Mon, Jan 30, 2023, 11:23 PM +05:30

--- javatpoint/practise_for_hackerrank.c
+++ javatpoint/practise_for_hackerrank.c
@@ -1,8 +1,10 @@
+
+/*
 #include<stdio.h>
 #include<stdbool.h>
 #include<string.h>
 
-/*
+
 bool checkPrime(int num)
 {
 	int flag = 0;
@@ -22,7 +24,7 @@ bool checkPrime(int num)
 		return false;
 	}
 }
-*/
+
 
 int main(void)
 {
@@ -35,4 +37,30 @@ int main(void)
 	}
 	printf("\n");
 	return 0;
+}
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+    char mystrg[60];
+    int leng, g;
+
+    // Printing the program name and what the program will do
+    printf("Program in C for reversing a given string \n ");
+    printf("Please insert the string you want to reverse: ");
+
+    // fetch the input string from the user
+    scanf( "%s", mystrg );
+
+    // This will find the length of your string with the help of strlen() function of string.h header file
+    leng = strlen(mystrg);
+
+    // iterate through each and every character of the string for printing it backwards or reverse direction
+    for(g = leng - 1; g >= 0; g--) {
+        printf("%c", mystrg[g]);
+    }
+    return 0;
 }
