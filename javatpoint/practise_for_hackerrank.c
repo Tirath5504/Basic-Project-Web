c6ac100ec53fbb02d4b8be9c9d5dc8916c7c123f
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-02-13 19:00:18 +0000

    Tue, Feb 14, 2023, 12:30 AM +05:30

--- javatpoint/practise_for_hackerrank.c
+++ javatpoint/practise_for_hackerrank.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
-void nat(int n1, int n2);
+int nat(int n1, int n2);
 
 int main()
 {
-    int num1, num2;
+    int num1, num2, sum;
     printf("Enter range: ");
     scanf("%d %d", &num1, &num2);
-    nat(num1, num2);
+    sum = nat(num1, num2);
+	printf("Sum = %d", sum);
     return 0;
 }
 
-void nat(int n1, int n2)
+int nat(int n1, int n2)
 {
 	int sum = 0;
-	if (n1 != n2)
+	if (n1 <= n2)
 	{
-		printf("%d", n1);
-		nat(n1+1, n2);
+		return (sum + nat(n1+1, n2));
 	}
 	else
 	{
-		return
+		return sum;
 	}
 }
