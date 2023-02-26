ac00576d674b1e2997f4f78e7fd026ce02678fc7
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-02-26 16:54:04 +0000

    Sun, Feb 26, 2023, 10:24 PM +05:30

--- javatpoint/practise_for_hackerrank.c
+++ javatpoint/practise_for_hackerrank.c
@@ -2,11 +2,32 @@
 
 #define MAX 10
 
-int a[MAX][MAX], b[MAX][MAX];
+int m1[MAX][MAX], m2[MAX][MAX], res[MAX][MAX];
 
-void input(int mat[MAX][MAX])
+void input(int mat[MAX][MAX], int rows, int columns)
 {
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			printf("Enter element (%d, %d): ", i+1, j+1);
+			scanf("%d", &mat[i][j]);
+		}
+	}
+}
 
+void multiply(int mat1[MAX][MAX], int mat2[MAX][MAX], int rows, int columns)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			for (int k = 0; k < columns; k++)
+			{
+				res
+			}
+		}
+	}
 }
 
 int main(void)
@@ -14,7 +35,14 @@ int main(void)
 	int n1a, n1b, n2a, n2b;
 	printf("Enter order of Matrix 1: ");
 	scanf("%d %d", &n1a, &n1b);
-	
+	input(m1[][], n1a, n1b);
+	printf("Enter order of Matrix 2: ");
+	scanf("%d %d", &n2a, &n2b);
+	input(m2[][], n2a, n2b);
+	if (n1b == n2a)
+		multiply(m1[][], m2[][], n1a, n1b);
+	else
+		printf("Matrixes not compatible for multiplication");
 	printf("\n");
 	return 0;
 }
