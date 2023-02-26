9570cab2624c509532f32e627f7ec877199422c1
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-02-26 17:01:17 +0000

    Sun, Feb 26, 2023, 10:31 PM +05:30

--- javatpoint/practise_for_hackerrank.c
+++ javatpoint/practise_for_hackerrank.c
@@ -2,7 +2,7 @@
 
 #define MAX 10
 
-int m1[MAX][MAX], m2[MAX][MAX], res[MAX][MAX];
+int res[MAX][MAX];
 
 void input(int mat[MAX][MAX], int rows, int columns)
 {
@@ -14,6 +14,7 @@ void input(int mat[MAX][MAX], int rows, int columns)
 			scanf("%d", &mat[i][j]);
 		}
 	}
+	printf("\n");
 }
 
 void multiply(int mat1[MAX][MAX], int mat2[MAX][MAX], int rows, int columns)
@@ -24,23 +25,40 @@ void multiply(int mat1[MAX][MAX], int mat2[MAX][MAX], int rows, int columns)
 		{
 			for (int k = 0; k < columns; k++)
 			{
-				res
+				res[i][j] += mat1[i][k] * mat2[k][j];
 			}
 		}
+	}
+	printf("\n");
+}
+
+void print(int mat[MAX][MAX], int rows, int columns)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			printf("%d\t", mat[i][j]);
+		}
+		printf("\n");
 	}
 }
 
 int main(void)
 {
 	int n1a, n1b, n2a, n2b;
+	int m1[MAX][MAX], m2[MAX][MAX];
 	printf("Enter order of Matrix 1: ");
 	scanf("%d %d", &n1a, &n1b);
-	input(m1[][], n1a, n1b);
+	input(m1[n1a][n1b], n1a, n1b);
 	printf("Enter order of Matrix 2: ");
 	scanf("%d %d", &n2a, &n2b);
-	input(m2[][], n2a, n2b);
+	input(m2[n2a][n2b], n2a, n2b);
 	if (n1b == n2a)
-		multiply(m1[][], m2[][], n1a, n1b);
+	{
+		multiply(m1[n1a][n1b], m2[n2a][n2b], n1a, n2b);
+		print(res[n1a][n2b], n1a, n2b);
+	}
 	else
 		printf("Matrixes not compatible for multiplication");
 	printf("\n");
