45a0bbcdd4d1a934f20e068acb3003eeede6b964
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-02-27 06:10:14 +0000

    Mon, Feb 27, 2023, 11:40 AM +05:30

--- javatpoint/practise_for_hackerrank.c
+++ javatpoint/practise_for_hackerrank.c
@@ -153,7 +153,7 @@ int main(void)
 	}
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < n-1; j++)
 		{
 			if ((s[j+1].m1 + s[j+1].m2 + s[j+1].m3) > (s[j].m1 + s[j].m2 + s[j].m3))
 			{
