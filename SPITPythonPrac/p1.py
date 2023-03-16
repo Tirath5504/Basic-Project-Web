b582b8ac9087d186745be8cb380b6442c1932a24
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-03-16 11:59:16 +0000

    Thu, Mar 16, 2023, 5:29 PM +05:30

--- SPITPythonPrac/p1.py
+++ SPITPythonPrac/p1.py
@@ -4,8 +4,16 @@ def main():
     string()
 
 def string():
-    name = "Tirath"
+    name = "Tirath Bhathawala"
     print(name)
     print(type(name))
-    
+    print(name[2:5])
+    print(len(name))
+    print(name[:4])
+    print(name[3:])
+    print(name[-1])
+    print(name[2:-2])
+    print(name[2:9:2])
+    print(name[::-1])
+
 main()
