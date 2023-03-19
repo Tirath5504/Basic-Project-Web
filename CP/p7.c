7da5f04567468951a5b86c51dd1b7182c565a75e
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-03-19 11:20:24 +0000

    Sun, Mar 19, 2023, 4:50 PM +05:30

---
+++ CP/p7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n1, n2, n, flag = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        scanf("%d", &num);
+        char str[num];
+        scanf("%s", &str);
+        for(int j = 0, k = num-1; j < num; j++, k--)
+        {
+            
+        }
+    }
+    return 0;
+}
