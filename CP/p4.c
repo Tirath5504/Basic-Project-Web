24d9417a42dfc35c5469b1e03d80c4dbf1c1cf47
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-03-19 10:56:03 +0000

    Sun, Mar 19, 2023, 4:26 PM +05:30

--- CP/p4.c
+++ CP/p4.c
@@ -7,30 +7,40 @@ int main(void)
     int num[n];
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &num[i]);
-    }
-    int a = 0;
-    for (int i = 0; i < num[i]; i++)
-    {
-        no[i] = i;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        if (num[a] % 2 == 1 && num[a] > 3)
+        scanf("%d", num);
+        int a = 0;
+        for (int i = 0; i < num; i++)
         {
-            printf("3");
-            for (int j = 1; j < 2; j++)
+            no[i] = i;
+        }
+        for (int i = 0; i < num; i++)
+        {
+            if (num[a] % 2 == 1 && num[a] >= 3)
             {
-                printf("%d", j);
+                printf("3");
+                printf("1");
+                printf("2");
+                for (int j = 4; j <= num[a]; j += 2)
+                {
+                    int temp = no[j+1];
+                    no[j+1] = no[j];
+                    no[j] = temp;
+                }
             }
-            for (int j = 4; j < num[a]; j += 2)
+            else if (num[a] % 2 == 0)
             {
-                int temp = no[j+1];
-                no[j+1] = no[j];
-                no[j] = temp;
+                for (int j = 1; j <= num[a]; j += 2)
+                {
+                    int temp = no[j+1];
+                    no[j+1] = no[j];
+                    no[j] = temp;
+                }
             }
         }
-        printf()
+        for (int i = 0; i < num; i++)
+        {
+            printf("%d", no[i]);
+        }
         printf("\n");
         a++;
     }
