f1569b5c68817417cccd0e0a66199643590977d3
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-03-19 06:05:03 +0000

    Sun, Mar 19, 2023, 11:35 AM +05:30

--- codeforces/pset1-watermelon.c
+++ codeforces/pset1-watermelon.c
@@ -5,7 +5,7 @@ int main(void)
     int n;
     printf("Enter a number: ");
     scanf("%d", &n);
-    if (n % 2 == 0)
+    if (n % 2 == 0 && n != 2)
         printf("YES\n");
     else
         printf("NO\n");
