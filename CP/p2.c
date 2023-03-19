3edcfe0d44c0d4c229c8cdbac163d2c8dee5dab7
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-03-19 08:45:00 +0000

    Sun, Mar 19, 2023, 2:15 PM +05:30

--- CP/p2.c
+++ CP/p2.c
@@ -5,9 +5,11 @@ int main(void)
     int n1, n2, n;
     scanf("%d", &n);
     int num[n];
+    char str[n];
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &num[i]);
+        scanf("%s", &str[i])
     }
     
     return 0;
