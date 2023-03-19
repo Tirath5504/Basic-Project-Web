bba531b664b0d76b80d5a5be9edc37b667247e70
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-03-19 09:46:59 +0000

    Sun, Mar 19, 2023, 3:16 PM +05:30

--- CP/p3.c
+++ CP/p3.c
@@ -2,24 +2,24 @@
 #include <string.h>
 int main(void)
 {
-    int n1, n2, n, flag = 0;
+    int n, flag = 0;
     scanf("%d", &n);
     int num[n];
-    int n[1000];
+    int no[1000];
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &num[i]);
         for (j = 0; j < num[i]; j++)
         {
-            scanf("%d", &n[j]);
+            scanf("%d", &no[j]);
         }
     }
     for (int i = 0; i < n; i++)
     {
-        max = 
+        int max = no[0];
         for (j = 1; j < num[i]; j++)
         {
-            max =
+            max = 
         }
     }
 
