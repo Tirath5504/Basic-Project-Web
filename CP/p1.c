f477dc6e173b19213721f0536091c80ca3a15877
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-03-19 08:41:03 +0000

    Sun, Mar 19, 2023, 2:11 PM +05:30

--- CP/p1.c
+++ CP/p1.c
@@ -2,22 +2,26 @@
 
 int main(void)
 {
-    int num[], n1, n2, n;
+    int n1, n2, n;
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++)
+    int num[n];
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d", &num);
-        if (num % 3 == 0)
+        scanf("%d", &num[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (num[i] % 3 == 0)
         {
-            printf("%d %d", (num / 3), (num / 3));
+            printf("%d %d\n", (num[i] / 3), (num[i] / 3));
         }
-        else if (num % 3 == 1)
+        else if (num[i] % 3 == 1)
         {
-            printf("%d %d", ((num / 3) + 1), (num / 3));
+            printf("%d %d\n", ((num[i] / 3) + 1), (num[i] / 3));
         }
         else
         {
-            printf("%d %d", (num / 3), ((num / 3) + 1));
+            printf("%d %d\n", (num[i] / 3), ((num[i] / 3) + 1));
         }
     }
     return 0;
