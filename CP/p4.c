c29568909f9a98f77678c4cbcedfd926024851b8
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-03-19 10:44:12 +0000

    Sun, Mar 19, 2023, 4:14 PM +05:30

--- CP/p4.c
+++ CP/p4.c
@@ -12,16 +12,22 @@ int main(void)
     int a = 0;
     for (int i = 0; i < n; i++)
     {
+        for (int )
+    }
+    for (int i = 0; i < n; i++)
+    {
         if (num[a] % 2 == 1 && num[a] > 3)
         {
-            for (int j = 1; j < 3; j++)
+            printf("3");
+            for (int j = 1; j < 2; j++)
             {
                 printf("%d", j);
             }
             for (int j = 4; j < num[a]; j++)
             {
                 int temp = j+1;
-                
+                j+1 =
+
             }
         }
         printf("\n");
