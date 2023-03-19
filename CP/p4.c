bc9b2e02b931f3f997342a91d377c349e0df2c17
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-03-19 10:38:15 +0000

    Sun, Mar 19, 2023, 4:08 PM +05:30

--- CP/p4.c
+++ CP/p4.c
@@ -13,9 +13,9 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
         for (int j = 1; j < num[a]; j++)
-        if ()
+        if (num[a] % 2 == 0)
         {
-            printf(" %d", j);
+            printf();
         }
         printf("\n");
         a++;
