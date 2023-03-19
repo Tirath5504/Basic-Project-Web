9bbf784b8cc8fe95e29d1cf0b75f0f88212f2fe2
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-03-19 08:37:23 +0000

    Sun, Mar 19, 2023, 2:07 PM +05:30

--- CP/p1.c
+++ CP/p1.c
@@ -2,6 +2,23 @@
 
 int main(void)
 {
-    
+    int num[], n1, n2, n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &num);
+        if (num % 3 == 0)
+        {
+            printf("%d %d", (num / 3), (num / 3));
+        }
+        else if (num % 3 == 1)
+        {
+            printf("%d %d", ((num / 3) + 1), (num / 3));
+        }
+        else
+        {
+            printf("%d %d", (num / 3), ((num / 3) + 1));
+        }
+    }
     return 0;
 }
