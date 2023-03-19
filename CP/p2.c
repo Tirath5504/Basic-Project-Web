604d000e50f88a7a60abef9c734037802b6606c0
Tirath5504 <111230032+Tirath5504@users.noreply.github.com>
2023-03-19 08:50:51 +0000

    Sun, Mar 19, 2023, 2:20 PM +05:30

--- CP/p2.c
+++ CP/p2.c
@@ -2,15 +2,30 @@
 
 int main(void)
 {
-    int n1, n2, n;
+    int n1, n2, n, flag = 0;
     scanf("%d", &n);
     int num[n];
     char str[n];
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &num[i]);
-        scanf("%s", &str[i])
+        scanf("%s", &str[i]);
     }
-    
+    for (int i = 0; i < n; i++)
+    {
+        if (str[i+1] != str[i])
+        {
+            for (int j = i+2; j < n; j++)
+            {
+                if (str[i] == str[j])
+                {
+                    printf("NO");
+                    flag = 1;
+                }
+            }
+        }
+        if (!flag)
+            printf("YES");
+    }
     return 0;
 }
